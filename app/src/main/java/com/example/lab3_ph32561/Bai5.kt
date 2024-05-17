86f267cd1ba910c735754255b78587b9f60df0a3
Admin <hiuteddy204@gmail.com>
2024-05-17 17:15:46 +0700

    bài 5

--- app/src/main/java/com/example/lab3_ph32561/Bai5.kt
+++ app/src/main/java/com/example/lab3_ph32561/Bai5.kt
@@ -7,15 +7,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-<<<<<<< HEAD
-=======
-import androidx.compose.foundation.magnifier
->>>>>>> 75cf113 (bài 5)
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -23,10 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-<<<<<<< HEAD
-=======
 import androidx.compose.ui.res.painterResource
->>>>>>> 75cf113 (bài 5)
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,83 +44,6 @@ class Bai5 : ComponentActivity() {
 @Composable
 fun PaymentScreen() {
     val paymentMethods = listOf(
-<<<<<<< HEAD
-        PaymentMethod("PayPal", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/PayPal_logo.svg/512px-PayPal_logo.svg.png", Color(0xFFFF9800)),
-        PaymentMethod("VISA", "https://upload.wikimedia.org/wikipedia/commons/0/04/Visa.svg", Color(0xFF1434CB)),
-        PaymentMethod("Momo", "https://upload.wikimedia.org/wikipedia/commons/3/3c/MoMo_Logo.png", Color(0xFFE91E63)),
-        PaymentMethod("Zalo Pay", "https://upload.wikimedia.org/wikipedia/commons/7/7b/ZaloPay_Logo.png", Color(0xFF03A9F4)),
-        PaymentMethod("Thanh toán trực tiếp", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Cash_icon.svg/1024px-Cash_icon.svg.png", Color(0xFF00E676))
-    )
-
-    val selectedMethod = remember { mutableStateOf("") }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Text(
-            text = "Thanh Toán",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(top = 16.dp)
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = "Địa chỉ nhận hàng",
-            color = Color.White,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
-        Text(
-            text = "Trí | 2222222\n22/333 đường Trung Mỹ Tây 1\nphường Tân Thới Nhất\nquận 12, Thành phố Hồ Chí Minh",
-            color = Color.White,
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Text(
-            text = "Vui lòng chọn một trong những phương thức sau:",
-            color = Color.White,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
-
-        Spacer(modifier = Modifier.height(8.dp))
-
-        paymentMethods.forEach { method ->
-            PaymentOption(
-                method = method,
-                isSelected = selectedMethod.value == method.name,
-                onSelect = { selectedMethod.value = method.name }
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-        }
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Button(
-            onClick = { /* Handle button click */ },
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF7043)),
-            shape = RoundedCornerShape(10.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Text(
-                text = "Tiếp theo",
-                fontSize = 18.sp,
-                color = Color.White,
-                modifier = Modifier.padding(8.dp)
-            )
-        }
-
-=======
         PaymentMethod(
             "PayPal",
             "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/PayPal_logo.svg/512px-PayPal_logo.svg.png",
@@ -160,121 +78,114 @@ fun PaymentScreen() {
     )
 
     val selectedMethod = remember { mutableStateOf("") }
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFF221919)) // Màu xám đậm sử dụng mã hex
-    ) {
-        Column(
+
+    Scaffold(
+        bottomBar = { BottomNavigationBar() }
+    ) { innerPadding ->
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(5.dp) // Padding cho toàn bộ nội dung
-
-
+                .background(Color(0xFF333333)) // Màu xám đậm sử dụng mã hex
+                .padding(innerPadding)
         ) {
-            Text(
-                text = "Thanh Toán",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White,
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(top = 10.dp)
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-            Spacer(modifier = Modifier.height(4.dp))
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(2.dp)
-                    .background(color = Color.White)
-            )
-
-            Text(
-                text = "Địa chỉ nhận hàng",
-                fontSize = 16.sp,
-                color = Color.White,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(top = 16.dp, start = 6.dp)
-            )
-            Spacer(modifier = Modifier.height(10.dp))
-
-            Row(
-                modifier = Modifier.padding(horizontal = 14.dp),
-                verticalAlignment = Alignment.CenterVertically
-            )
-
-            {
-                CoilImage(
-                    data = "https://cdn-icons-png.flaticon.com/128/684/684908.png", // Thay "url_của_bạn" bằng đường dẫn URL thực của hình ảnh của bạn
-                    contentDescription = "Your Image Description",
-                    modifier = Modifier.size(40.dp) // Điều chỉnh kích thước của hình ảnh theo nhu cầu
-                )
-                Text(
-                    fontSize = 14.sp,
-                    text = "Trí | 2222222\n22/333 đường Trung Mỹ Tây 1\nphường Tân Thới Nhất\nquận 12, Thành phố Hồ Chí Minh",
-                    color = Color.White,
-                    modifier = Modifier.padding(horizontal = 16.dp)
-                )
-            }
-
-
-
-            Spacer(modifier = Modifier.height(14.dp))
-
-            Text(
-                text = "Vui lòng chọn một trong những phương thức sau:",
-                color = Color.White,
-                fontSize = 14.sp,
-                modifier = Modifier.padding(horizontal = 10.dp)
-            )
-
-            Spacer(modifier = Modifier.height(20.dp))
-
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(15.dp) // Padding cho toàn bộ nội dung
-
-
+                    .padding(5.dp) // Padding cho toàn bộ nội dung
             ) {
-                paymentMethods.forEach { method ->
-                    PaymentOption(
-                        method = method,
-                        isSelected = selectedMethod.value == method.name,
-                        onSelect = { selectedMethod.value = method.name }
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                }
+                Text(
+                    text = "Thanh Toán",
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = 25.dp, bottom = 20.dp)
+                )
 
-                Spacer(modifier = Modifier.weight(1f))
-                Button(
-                    onClick = { /* Handle button click */ },
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF7043)),
-                    shape = RoundedCornerShape(10.dp),
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Box(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(10.dp)
+                        .height(2.dp)
+                        .background(color = Color.White)
+                )
+
+                Text(
+                    text = "Địa chỉ nhận hàng",
+                    fontSize = 18.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(horizontal = 14.dp, vertical = 10.dp)
+                )
+
+                Row(
+                    modifier = Modifier.padding(horizontal = 14.dp),
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
+                    CoilImage(
+                        data = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png", // Thay "url_của_bạn" bằng đường dẫn URL thực của hình ảnh của bạn
+                        contentDescription = "Location Icon",
+                        modifier = Modifier.size(40.dp) // Điều chỉnh kích thước của hình ảnh theo nhu cầu
+                    )
                     Text(
-                        text = "Tiếp theo",
-                        fontSize = 14.sp,
+                        fontSize = 15.sp,
+                        text = "Trí | 2222222\n22/333 đường Trung Mỹ Tây 1\nphường Tân Thới Nhất\nquận 12, Thành phố Hồ Chí Minh",
                         color = Color.White,
-                        modifier = Modifier.padding(8.dp)
+                        modifier = Modifier.padding(horizontal = 16.dp)
                     )
                 }
+
+                Spacer(modifier = Modifier.height(14.dp))
+
+                Text(
+                    text = "Vui lòng chọn một trong những phương thức sau:",
+                    color = Color.White,
+                    fontSize = 15.sp,
+                    modifier = Modifier.padding(horizontal = 10.dp)
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(15.dp) // Padding cho toàn bộ nội dung
+                ) {
+                    paymentMethods.forEach { method ->
+                        PaymentOption(
+                            method = method,
+                            isSelected = selectedMethod.value == method.name,
+                            onSelect = { selectedMethod.value = method.name }
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    Button(
+                        onClick = { /* Handle button click */ },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF7043)),
+                        shape = RoundedCornerShape(10.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 35.dp, start = 15.dp, end = 15.dp)
+                    ) {
+                        Text(
+                            text = "Tiếp theo",
+                            fontSize = 14.sp,
+                            color = Color.White,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
+                }
             }
-
-
         }
->>>>>>> 75cf113 (bài 5)
     }
 }
 
 @Composable
-<<<<<<< HEAD
-=======
 fun CoilImage(data: String, contentDescription: String, modifier: Modifier) {
     Image(
         painter = rememberImagePainter(data),
@@ -283,9 +194,7 @@ fun CoilImage(data: String, contentDescription: String, modifier: Modifier) {
     )
 }
 
-
 @Composable
->>>>>>> 75cf113 (bài 5)
 fun PaymentOption(
     method: PaymentMethod,
     isSelected: Boolean,
@@ -297,24 +206,15 @@ fun PaymentOption(
             .fillMaxWidth()
             .border(2.dp, Color.Gray, RoundedCornerShape(10.dp))
             .background(color = method.color, shape = RoundedCornerShape(10.dp))
-<<<<<<< HEAD
-            .padding(16.dp)
-            .height(35.dp) // 3Ensure consistent height
-=======
             .padding(20.dp)
-            .height(33.dp) // 3Ensure consistent height
->>>>>>> 75cf113 (bài 5)
+            .height(36.dp) // Ensure consistent height
     ) {
         Image(
             painter = rememberImagePainter(data = method.imageUrl),
             contentDescription = method.name,
             contentScale = ContentScale.Fit,
             modifier = Modifier
-<<<<<<< HEAD
-                .size(40.dp) // Adjust size for visibility
-=======
                 .size(50.dp) // Adjust size for visibility
->>>>>>> 75cf113 (bài 5)
                 .padding(end = 16.dp) // Padding to separate image and text
         )
         Text(
@@ -324,17 +224,44 @@ fun PaymentOption(
             modifier = Modifier
                 .weight(1f)
                 .align(Alignment.CenterVertically) // Canh giữa theo chiều dọc
-                .padding(start = 80.dp), // Cách lề 30 dp
-
-<<<<<<< HEAD
-            )
-=======
+                .padding(start = 80.dp) // Cách lề 30 dp
         )
->>>>>>> 75cf113 (bài 5)
         RadioButton(
             selected = isSelected,
             onClick = onSelect,
             modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
+
+@Composable
+fun BottomNavigationBar() {
+    NavigationBar(
+        containerColor = Color.Black
+    ) {
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Home, contentDescription = "Trang chủ") },
+            label = { Text("Trang chủ", color = Color.White) },
+            selected = false,
+            onClick = { /* TODO: Handle navigation */ }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Favorite, contentDescription = "Lịch sử") },
+            label = { Text("Lịch sử", color = Color.White) },
+            selected = false,
+            onClick = { /* TODO: Handle navigation */ }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.ShoppingCart, contentDescription = "Giỏ hàng") },
+            label = { Text("Giỏ hàng", color = Color.White) },
+            selected = true, // Item đang được chọn
+            onClick = { /* TODO: Handle navigation */ }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Person, contentDescription = "Hồ sơ") },
+            label = { Text("Hồ sơ", color = Color.White) },
+            selected = false,
+            onClick = { /* TODO: Handle navigation */ }
         )
     }
 }
